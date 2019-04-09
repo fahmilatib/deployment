23ddf5a2390915af44bf349069b817172075db5f
Muhammad Zulfahmi bin Ab Latif <zulfahmi@ecosystem.natsoft.com.my>
2019-04-09 13:46:38 +0800

    Add backup script

---
+++ backup.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+DEPLOYMENT_ID=foobar
+BASE_BACKUP_DIR=/root/backup
+BACKUP_DIR=$BASE_BACKUP_DIR/$DEPLOYMENT_ID
+
+if [ -f $BACKUP_DIR.tar.gz ]; then
+    echo
+    echo "Backup has been performed earlier"
+    exit 1
+fi
+
+if [ ! -d $BASE_BACKUP_DIR ]; then
+    mkdir $BASE_BACKUP_DIR
+fi
+
+mkdir $BACKUP_DIR
+
+echo
+echo 'Backing up...'
+echo
+
+cp --parents -v /root/app/contact.php $BACKUP_DIR
+cp --parents -v /root/app/index.php $BACKUP_DIR
+
+echo
+echo 'Compressing...'
+echo
+
+tar -C $BASE_BACKUP_DIR -czvf $BACKUP_DIR.tar.gz $DEPLOYMENT_ID
+
+# clean up
+
+rm -rf $BACKUP_DIR
+
+echo
+echo 'Done'
+echo "$BACKUP_DIR.tar.gz"
+exit 0
