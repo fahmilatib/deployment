5877e93802f0ca4ce241be5f616bb588a111983f
Muhammad Zulfahmi bin Ab Latif <zulfahmi@ecosystem.natsoft.com.my>
2019-04-09 18:54:46 +0800

    Add rollback script

---
+++ rollback.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+DEPLOYMENT_ID=foobar
+BASE_BACKUP_DIR=/root/backup
+BACKUP_DIR=$BASE_BACKUP_DIR/$DEPLOYMENT_ID
+
+if [ ! -f $BACKUP_DIR.tar.gz ]; then
+    echo
+    echo "Nothing to rollback"
+    exit 1
+fi
+
+echo
+echo 'Decompressing...'
+echo
+
+tar -C $BASE_BACKUP_DIR -xvf $BACKUP_DIR.tar.gz
+
+echo
+echo 'Rolling back...'
+echo
+
+# restore old files
+
+cp -rv --preserve=mode,ownership $BACKUP_DIR/. /
+
+# delete newly added files
+
+rm -f /root/app/about.php
+rm -f /root/app/controllers/admin/Home.php
+
+# delete newly added directories
+
+rm -rf /root/app/controllers
+
+# clean up
+
+rm -rf $BACKUP_DIR
+
+echo
+echo 'Done'
+exit 0
